36fb1736575dc8cba6f7bb7668620305408b5140
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-10-05 13:20:44 +0545

    style: update active page behavior and add responsive design

--- assets/js/navbar/active.page.js
+++ assets/js/navbar/active.page.js
@@ -2,9 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const currentPath = window.location.pathname;
     const navLinks = document.querySelectorAll("nav ul li a");
 
+    const activeColor = document
+        .querySelector("nav")
+        .getAttribute("active-page-color");
+
     navLinks.forEach((link) => {
         if (link.getAttribute("href") === currentPath) {
-            link.style.fontWeight = "bold";
+            link.style.color = activeColor;
         }
     });
 });
